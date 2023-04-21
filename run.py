ec2d4a25da3cbeee5118c79e430e56ad7ee73891
Fictiverse <111762798+Fictiverse@users.noreply.github.com>
2023-04-21 04:25:08 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,21 @@
+from bark import SAMPLE_RATE, generate_audio
+from IPython.display import Audio
+from scipy.io.wavfile import write as write_wav
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--prompt', default='Hello world [laughs]', help='The text prompt to use')
+parser.add_argument('--voice', default='auto', help='The voice to use')
+args = parser.parse_args()
+
+text_prompt = '"""\n' + args.prompt + '\n"""'
+voice = args.voice
+
+if voice == "auto":
+    audio_array = generate_audio(text_prompt)
+else
+    audio_array = generate_audio(text_prompt, history_prompt=voice)
+
+Audio(audio_array, rate=SAMPLE_RATE)
+
+write_wav("audio.wav", SAMPLE_RATE, audio_array)
