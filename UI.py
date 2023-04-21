e1e3fadf2dbd5531edb01677a17ac4cd8f5f4297
Fictiverse <111762798+Fictiverse@users.noreply.github.com>
2023-04-21 10:06:02 +0200

    Gradio WebUI

--- UI.py
+++ UI.py
@@ -5,36 +5,46 @@ import gradio as gr
 import os 
 
 voicePath = "bark/assets/prompts";
-npz_files = [file for file in os.listdir(voicePath) if file.endswith(".npz")]
-npz_files = [os.path.splitext(os.path.basename(file))[0] for file in npz_files]
-npz_files.insert(0, "auto")
+files = os.listdir(voicePath)
+npz_files = [f for f in files if f.endswith(".npz")]
+npz_names = [os.path.splitext(f)[0] for f in npz_files]
+
+print("\nStarting Suno-AI bark TTS WebUI\n")
 
 def start(prompt, voice):
-    if voice == "auto":
+    print("\nStarting...")
+    print(" Prompt : " + prompt)
+    print(" Voice : " + (npz_names[voice] if voice is not None else "Default"))
+    print()
+    #prompt = '\n'.join(prompts)
+    
+    if voice == None:
         audio_array = generate_audio(prompt)
-    else:
-        audio_array = generate_audio(prompt, history_prompt=voice)
-    audio_array = generate_audio(prompt, history_prompt=voice)
+    else:   
+        audio_array = generate_audio(prompt, history_prompt=npz_names[voice])
+        
     write_wav("audio.wav", SAMPLE_RATE, audio_array)
     return "audio.wav"
 
 with gr.Blocks() as demo:
+
     gr.Title="Bark TTS WebUI",
     gr.Markdown("Bark TTS WebUI")
+    
     with gr.Row():
 
-        prompt = gr.Textbox(label="Prompt", placeholder="Enter your text here", lines=4)
+        prompts = gr.Textbox(label="Prompt", placeholder="Enter your text here", lines=4)
 
     with gr.Row():
-        voice = gr.Dropdown(npz_files, value=["auto"], label="Voice", info="Select the voice")
+        voice = gr.Dropdown(npz_names, type="index", label="Voice", info="Select the voice")
         launch_button = gr.Button("Launch")   
   
-    with gr.Column(visible=True):
+    with gr.Column():
         output = gr.Audio(label="Result")
         
     launch_button.click(
         start,
-        [prompt, voice],
+        [prompts, voice],
         [output],
     )        
 
