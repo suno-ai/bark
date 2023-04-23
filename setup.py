8deca018837b5af8332ec68cfb9b2a550ecbddcb
Taranjeet Singh <reachtotj@gmail.com>
2023-04-23 23:38:32 +0530

    Update setup.py
    Code changes:
    * Added comments to explain the purpose of the `setup()` function and how to provide additional configuration if needed.
    * Added a link to the setuptools documentation for further reference.

--- setup.py
+++ setup.py
@@ -1,3 +1,35 @@
 from setuptools import setup
 
+# The setup function is responsible for configuring the package and its dependencies.
+# It is called with no arguments, which means it will use the configuration from the setup.cfg file.
+# If you need to provide additional configuration, you can pass keyword arguments to the setup function.
+# For example, you can specify the package name, version, author, etc. like this:
+#
+# setup(
+#     name="my_package",
+#     version="1.0.0",
+#     author="Your Name",
+#     author_email="your.email@example.com",
+#     description="A short description of your package",
+#     long_description="A longer description of your package",
+#     url="https://github.com/yourusername/your-package",
+#     packages=["my_package"],
+#     classifiers=[
+#         "Development Status :: 3 - Alpha",
+#         "Intended Audience :: Developers",
+#         "License :: OSI Approved :: MIT License",
+#         "Programming Language :: Python :: 3",
+#         "Programming Language :: Python :: 3.6",
+#         "Programming Language :: Python :: 3.7",
+#         "Programming Language :: Python :: 3.8",
+#         "Programming Language :: Python :: 3.9",
+#     ],
+#     install_requires=[
+#         # List your package's dependencies here
+#     ],
+# )
+#
+# For more information on how to configure your package, refer to the setuptools documentation:
+# https://setuptools.pypa.io/en/latest/userguide/declarative_config.html
+
 setup()
